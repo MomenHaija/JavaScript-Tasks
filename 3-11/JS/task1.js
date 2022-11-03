90020094ebabde96d825eac252dc07d6298f46e0
momen Abu Alhaija <115459911+MomenHaija@users.noreply.github.com>
2022-11-03 14:45:19 +0200

    first task in JS

---
+++ 3-11/JS/task1.js
@@ -0,0 +1,24 @@
+var x=89;
+
+if(x>100){
+    document.write("invalid Data"+"<br>");
+}
+else if(x<50){
+    document.write("FAIL"+"<br>")
+}
+else if(x>=50&&x<=59){
+    document.write("D"+"<br>");
+}
+else if(x>59&&x<=69){
+    document.write("C"+"<br>")
+}
+else if(x>69&&x<=79){
+    document.write("B"+"<br>")
+}
+else if(x>79&&x<=89){
+    document.write("A"+"<br>")
+}
+else{
+    document.write("A+"+"<br>")
+}
+
---
+++ 3-11/JS/task2.js
@@ -0,0 +1,4 @@
+const pi=3.14;
+let r=10;
+let circlearea=pi*r*r;
+document.write(circlearea);
